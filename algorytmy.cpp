740568c543fcc510812f526ba5beaa90d3a00117
Michael Moses <Michael@n90x.info>
2024-05-02 14:00:51 +0200

    Add header algortmy.h and a TODO list for algorytmy.cpp.

---
+++ algorytmy.cpp
@@ -0,0 +1,29 @@
+
+#include "headers/algortymy.h"
+#include <iostream>
+
+using namespace std;
+
+/*
+    TODO :
+        * Insert sort
+        * Bubble sort
+        * Selection sort
+
+        * Quick sort
+        * Heap sort
+        * Shell sort
+
+    
+    TODO extra: 
+        * Insert sort with guard
+        * Insert sort with binarry search
+
+        * Bubble sort - shaker
+
+        * Double Selection sort
+
+        * Quick sort - partition
+*/
+
+
---
+++ headers/algortymy.h
@@ -0,0 +1,9 @@
+#ifndef __algorytmy_h__
+#define __algorytmy_h__
+
+
+
+
+
+
+#endif //__algorytmy_h__
